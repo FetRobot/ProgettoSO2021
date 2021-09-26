5ad16e1845b4571e33e2da69d670c3644bf38eb9
Leonardo <61968271+FetRobot@users.noreply.github.com>
2021-09-26 20:08:17 +0200

    Added comments for reading pleasure

--- 08_disastrOS/disastrOS_04_resources/disastrOS_read_queue.c
+++ 08_disastrOS/disastrOS_04_resources/disastrOS_read_queue.c
@@ -9,19 +9,26 @@ void readQueue(){
 
   printf("READ\n");
 
+  //lf I read the input parameters
 	queue* q = running->syscall_args[0];
 	char* s = running->syscall_args[1];
 	int size = running->syscall_args[2];
 
   int i=0;
+  //lf if the available space to read is 0: return 0
+  //lf this way I can tell I read 0 characters
   if(q->readingSpace==0){
       running->syscall_retvalue=0;
       printQueue(q);
       return;
   }
+  //lf until there's space for reading and until I haven't finished reading the characters I need to read
   while(i<size && q->readingSpace>0){
+      //lf I read from the queue and I copy the read character in the string 's'
       s[i]=q->buffer[q->readingIndex];
+      //lf I put back the read character inside the queue with the default value: so I can visually see how the queue evolves
       q->buffer[q->readingIndex]='_';
+      //lf I update the input parameters
       q->readingIndex=(q->readingIndex+1)%100;
       q->readingSpace--;
       q->writingSpace++;
--- 08_disastrOS/disastrOS_04_resources/disastrOS_write_queue.c
+++ 08_disastrOS/disastrOS_04_resources/disastrOS_write_queue.c
@@ -9,18 +9,23 @@ void writeQueue(){
 
   printf("WRITE\n");
 
+  //lf I read and store the input paremeters
 	queue* q = running->syscall_args[0];
 	char* s = running->syscall_args[1];
 	int size = running->syscall_args[2];
 
   int i=0;
+  //lf if the available space is 0: return 0
+  //lf this way I can tell I wrote 0 characters
   if(q->writingSpace==0) {
       running->syscall_retvalue=0;
       printQueue(q);
       return;
   }
+  //lf until there's space for writing on and until I haven't finished writing the characters I need to write
   while(i<size && q->writingSpace>0){
-      q->buffer[q->writingIndex]=s[i];
+      q->buffer[q->writingIndex]=s[i]; //lf I write in the queue
+      //lf and I update the parameters accordingly
       i++;
       q->writingIndex=(q->writingIndex+1)%100;
       q->writingSpace--;
